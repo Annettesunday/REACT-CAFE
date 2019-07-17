0c9869f6ecd3dac8be719c764ad32dbeb9af5a04
annettesunday <annette.odhiambo@andela.com>
2019-07-17 12:55:36 +0300

    ch(Header): build header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MenuList from "./menus/MenuList";
 import MenuDelete from "./menus/MenuDelete";
 import MenuEdit from "./menus/MenuEdit";
 import MenuShow from "./menus/MenuShow";
 import MenuCreate from "./menus/MenuCreate";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div className="ui container">
       <BrowserRouter>
-        <Route path="/" exact component={MenuList} />
-        <Route path="/menus/new" component={MenuCreate} />
-        <Route path="/menus/delete/:id/" component={MenuDelete} />
-        <Route path="/menus/edit/:id" component={MenuEdit} />
-        <Route path="/menus/:id" component={MenuShow} />
+        <Header />
+        <Switch>
+          <Route path="/" exact component={MenuList} />
+          <Route path="/menus/new" component={MenuCreate} />
+          <Route path="/menus/delete/:id/" component={MenuDelete} />
+          <Route path="/menus/edit/:id" component={MenuEdit} />
+          <Route path="/menus/:id" component={MenuShow} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
