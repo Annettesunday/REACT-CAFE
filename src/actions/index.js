a05622bb6ee7b1a7e5f7a642a83be39e92db3336
annettesunday <annette.odhiambo@andela.com>
2019-07-18 10:26:56 +0300

    ch(actionTypes): centralize action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,12 @@
 import Menu from "../apis/Menu";
+import { SIGN_IN, SIGN_UP } from "./actionTypes";
 
 export const signUp = user => async dispatch => {
   try {
     const response = await Menu.post("/signup", user);
     console.log("The response is", response.data);
     dispatch({
-      type: "SIGN_UP",
+      type: SIGN_UP,
       payload: response.data
     });
   } catch (error) {
@@ -16,7 +17,7 @@ export const signIn = user => async dispatch => {
   try {
     const response = await Menu.post("/login", user);
     return dispatch({
-      type: "SIGN_IN",
+      type: SIGN_IN,
       payload: response.data
     });
   } catch (error) {
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,12 +1,13 @@
+import { SIGN_IN, SIGN_UP } from "../actions/actionTypes";
 const INITIAL_STATE = {
   token: null
 };
 
 const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SIGN_IN":
+    case SIGN_IN:
       return { ...state, token: action.payload };
-    case "SIGN_UP":
+    case SIGN_UP:
       return { ...state, token: action.payload };
     default:
       return state;
