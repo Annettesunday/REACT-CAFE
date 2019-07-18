25e7c1388a257e4a431fd2c07f4168799f609a4d
annettesunday <annette.odhiambo@andela.com>
2019-07-18 11:44:49 +0300

    ch(history): create browser history object for programmatic navigation

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import Menu from "../apis/Menu";
 import { SIGN_IN, SIGN_UP } from "./actionTypes";
+import history from "../history";
 
 export const signUp = user => async dispatch => {
   try {
@@ -9,6 +10,7 @@ export const signUp = user => async dispatch => {
       type: SIGN_UP,
       payload: response.data
     });
+    history.push("/");
   } catch (error) {
     console.log(error.response.data);
   }
@@ -16,10 +18,12 @@ export const signUp = user => async dispatch => {
 export const signIn = user => async dispatch => {
   try {
     const response = await Menu.post("/login", user);
-    return dispatch({
+    console.log("the response is", response.data)
+    dispatch({
       type: SIGN_IN,
       payload: response.data
     });
+    history.push("/");
   } catch (error) {
     console.log(error.response.data);
   }
--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import MenuList from "./menus/MenuList";
 import MenuDelete from "./menus/MenuDelete";
 import MenuEdit from "./menus/MenuEdit";
 import MenuShow from "./menus/MenuShow";
 import MenuCreate from "./menus/MenuCreate";
 import Header from "./Header";
-import Auth from "./Auth";
-
+import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
+import history from "../history"
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Switch>
           <Route path="/" exact component={MenuList} />
-          <Route path="/login" component={Auth} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/signup" component={SignupForm} />
           <Route path="/menus/new" component={MenuCreate} />
           <Route path="/menus/delete/:id/" component={MenuDelete} />
           <Route path="/menus/edit/:id" component={MenuEdit} />
           <Route path="/menus/:id" component={MenuShow} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
