a0a9907537f49bf01055125e9474d2c7222f068e
annettesunday <annette.odhiambo@andela.com>
2019-07-18 17:17:21 +0300

    ch(actions): set token to localstorage

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,19 @@
+import axios from "axios";
+
 import Menu from "../apis/Menu";
 import { SIGN_IN, SIGN_UP, SET_ERRORS } from "./actionTypes";
 import history from "../history";
 
+export const setAuthorizationToken = token => {
+  // Add bearer signature
+  const FBIToken = `Bearer ${token}`;
+
+  // set token to local storage which only stores strings
+  localStorage.setItem("jwtToken", FBIToken);
+
+  // set token to auth header
+  axios.defaults.headers.common["Authorization"] = FBIToken;
+};
 export const setErrors = errors => ({ type: SET_ERRORS, payload: errors });
 export const signUp = user => async dispatch => {
   try {
@@ -11,10 +23,11 @@ export const signUp = user => async dispatch => {
       type: SIGN_UP,
       payload: response.data
     });
+    setAuthorizationToken(response.data.token);
     history.push("/");
   } catch (error) {
-    // console.log(error.response.data);
-    dispatch(setErrors(error.response.data))
+    console.log(error.response.data);
+    dispatch(setErrors(error.response.data));
   }
 };
 export const signIn = user => async dispatch => {
@@ -25,10 +38,11 @@ export const signIn = user => async dispatch => {
       type: SIGN_IN,
       payload: response.data
     });
+    setAuthorizationToken(response.data.token);
     history.push("/");
   } catch (error) {
-    // console.log(error.response.data);
-    dispatch(setErrors(error.response.data))
+    console.log(error.response.data);
+    dispatch(setErrors(error.response.data));
   }
   console.log("finished");
 };
