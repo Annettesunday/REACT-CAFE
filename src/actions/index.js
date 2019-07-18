8504fb08a07f7b90a4fb49ddce14022866d92fd1
annettesunday <annette.odhiambo@andela.com>
2019-07-18 12:45:39 +0300

    ch(setErrors): create setErrors action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 import Menu from "../apis/Menu";
-import { SIGN_IN, SIGN_UP } from "./actionTypes";
+import { SIGN_IN, SIGN_UP, SET_ERRORS } from "./actionTypes";
 import history from "../history";
 
+export const setErrors = errors => ({ type: SET_ERRORS, payload: errors });
 export const signUp = user => async dispatch => {
   try {
     const response = await Menu.post("/signup", user);
@@ -12,20 +13,22 @@ export const signUp = user => async dispatch => {
     });
     history.push("/");
   } catch (error) {
-    console.log(error.response.data);
+    // console.log(error.response.data);
+    dispatch(setErrors(error.response.data))
   }
 };
 export const signIn = user => async dispatch => {
   try {
     const response = await Menu.post("/login", user);
-    console.log("the response is", response.data)
+    console.log("the response is", response.data);
     dispatch({
       type: SIGN_IN,
       payload: response.data
     });
     history.push("/");
   } catch (error) {
-    console.log(error.response.data);
+    // console.log(error.response.data);
+    dispatch(setErrors(error.response.data))
   }
   console.log("finished");
 };
