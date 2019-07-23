bb78aa81932b18715f6aba182f80c3f67b879347
annettesunday <annette.odhiambo@andela.com>
2019-07-23 13:07:46 +0300

    ch(actions): define logout action

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -2,3 +2,4 @@ export const SIGN_IN = "SIGN_IN";
 export const SIGN_UP = "SIGN_UP";
 export const SET_ERRORS = "SET_ERRORS";
 export const SET_USER = "SET_USER";
+export const SET_UNAUTHENTICATED = "SET_UNAUTHENTICATED";
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
 import Menu from "../apis/Menu";
-import { SIGN_IN, SIGN_UP, SET_ERRORS, SET_USER } from "./actionTypes";
+import {
+  SIGN_IN,
+  SIGN_UP,
+  SET_ERRORS,
+  SET_USER,
+  SET_UNAUTHENTICATED
+} from "./actionTypes";
 import history from "../history";
 
 export const setAuthorizationToken = token => {
@@ -12,6 +18,12 @@ export const setAuthorizationToken = token => {
   // set token to auth header
   Menu.defaults.headers.common["Authorization"] = FBIToken;
 };
+
+export const removeAuthorizationToken = () => {
+  localStorage.removeItem("jwtToken");
+  delete Menu.defaults.headers.common["Authorization"];
+};
+
 export const setErrors = errors => ({ type: SET_ERRORS, payload: errors });
 export const signUp = user => async dispatch => {
   try {
@@ -55,8 +67,15 @@ export const getUser = () => async dispatch => {
       type: SET_USER,
       payload: response.data
     });
-    console.log(response.data);
+    console.log("This is from the user method", response.data);
   } catch (error) {
     dispatch(setErrors(error.response.data));
   }
+};
+
+export const logout = () => dispatch => {
+  removeAuthorizationToken();
+  dispatch({
+    type: SET_UNAUTHENTICATED
+  });
 };
