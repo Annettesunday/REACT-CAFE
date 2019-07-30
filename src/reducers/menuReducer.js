aa9ee4b816e1a960f2a51495f7b14e2928feeb54
annettesunday <annette.odhiambo@andela.com>
2019-07-30 18:55:38 +0300

    ch(menuRedcuer): build menuReducer

--- src/reducers/menuReducer.js
+++ src/reducers/menuReducer.js
@@ -1,12 +1,15 @@
-import { CREATE_MENU_SUCCESSFULLY } from "../actions/actionTypes";
+import { CREATE_MENU_SUCCESSFULLY, FETCH_MENUS } from "../actions/actionTypes";
 
 const INITIAL_STATE = {
   menus: []
 };
+
 const menuReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CREATE_MENU_SUCCESSFULLY:
       return { menus: [action.payload, ...state.menus] };
+    case FETCH_MENUS:
+      return action.payload;
     default:
       return state;
   }
