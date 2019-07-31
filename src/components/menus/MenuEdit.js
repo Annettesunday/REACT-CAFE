07c11e2aaf501df7147763acae65cf884bb1b03c
annettesunday <annette.odhiambo@andela.com>
2019-07-31 12:11:38 +0300

    ch(MenuEdit): implement edit menu functionality

--- src/components/menus/MenuEdit.js
+++ src/components/menus/MenuEdit.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
+import { editMenu, fetchMenu } from "../../actions";
+import MenuForm from "./MenuForm";
 
-const MenuEdit = () => {
-  return <div>MenuEdit</div>;
+class MenuEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchMenu(this.props.match.params.id);
+    // console.log("I have been selected", this.props.match.params.id);
+  }
+
+  onSubmit = formValues => {
+    console.log("I have been selected", this.props.match.params.id);
+    console.log(" am getting hereI")
+
+    this.props.editMenu(this.props.match.params.id, formValues);
+  };
+  render() {
+    console.log("from state", this.props.menu.menuImage);
+    return (
+      <>
+        <h3>Edit A Menu</h3>
+        <MenuForm
+          onSubmit={this.onSubmit}
+          initialValues={_.pick(
+            this.props.menu,
+            "price",
+            "type",
+            "name",
+            "menuImage"
+          )}
+        />
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    menu: state.menus
+  };
 };
 
-export default MenuEdit;
+export default connect(
+  mapStateToProps,
+  { editMenu, fetchMenu }
+)(MenuEdit);
