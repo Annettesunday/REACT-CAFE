297f794c7fc71e6295613b362394d862980a2a68
annettesunday <annette.odhiambo@andela.com>
2019-08-01 14:58:56 +0300

    ch(Modal): customize Modal, and handle click events

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = () => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">I am a modal </div>
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={e => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">Delete Menu</div>
+        <div className="content">
+          Are you sure you want to delete this menu?
+        </div>
+        <div className="actions">
+          <button className="ui negative button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </div>
     </div>,
     document.querySelector("#modal")
   );
