5fc7fe5f4a1d54765a8fc471ece487d71614a079
annettesunday <annette.odhiambo@andela.com>
2019-08-01 21:31:32 +0300

    ch(Modal): create a reusable modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,24 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
-const Modal = () => {
+const Modal = (props) => {
   return ReactDOM.createPortal(
     <div
-      onClick={() => history.push("/")}
+      onClick={props.onDismiss}
       className="ui dimmer modals visible active"
     >
       <div
         onClick={e => e.stopPropagation()}
         className="ui standard modal visible active"
       >
-        <div className="header">Delete Menu</div>
+        <div className="header">{props.title}</div>
         <div className="content">
-          Are you sure you want to delete this menu?
+          {props.content}
         </div>
         <div className="actions">
-          <button className="ui negative button">Delete</button>
-          <button className="ui button">Cancel</button>
+          {props.actions}
         </div>
       </div>
     </div>,
--- src/components/menus/MenuDelete.js
+++ src/components/menus/MenuDelete.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../history";
 
 const MenuDelete = () => {
-  return <Modal />;
+  const actions = (
+    <>
+      <button className="ui negative button">Delete</button>
+      <button className="ui positive button">Cancel</button>
+    </>
+  );
+  return (
+    <Modal
+      title="Delete Menu"
+      content="Are you sure you want to delete this menu?"
+      actions={actions}
+      onDismiss={() => history.push("/")}
+    />
+  );
 };
 
 export default MenuDelete;
